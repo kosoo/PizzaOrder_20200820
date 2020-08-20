d54fe34638c27865071eaeeb1130e8122aca7fa9
이용찬 <lee9890@naver.com>
2020-08-20 21:21:43 +0900

    피자 가게 목록 프래그먼트 출현  -> 실 데이터 반영 X

--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/PizzaStoreFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/PizzaStoreFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_pizza_store.*
 import kr.co.tjoeun.pizzaorder_20200820.BaseActivity
 import kr.co.tjoeun.pizzaorder_20200820.R
+import kr.co.tjoeun.pizzaorder_20200820.adapters.StoreAdapter
 import kr.co.tjoeun.pizzaorder_20200820.datas.Store
 
 class PizzaStoreFragment : Fragment(){
 
     val mPizzaStoreList = ArrayList<Store>() //멤버변수
+
+    lateinit var mPizzaStoreAdapter : StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +33,12 @@ class PizzaStoreFragment : Fragment(){
         mPizzaStoreList.add(Store("파파존스","http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "1577-8080"))
         mPizzaStoreList.add(Store("도미노 피자","https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "1577-0077"))
         mPizzaStoreList.add(Store("피스터 피자","https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "1577-3082"))
+//        context 변수(Fragment 들이 갖고 있는 변수 - 우리가 만든 변수 아님) 로 대신 대입
+//        context 변수는 Context? 타입 => null일 수도 있다 => 어댑터 대입 불가
+//        !! 를 이용해, NullPointerException의 리스크를 감수하고 억지로 (null이 아니라고 ) 우겨서 대입
+        mPizzaStoreAdapter = StoreAdapter(context!!, R.layout.store_list_item, mPizzaStoreList)
 
-        //mvpa = MainViewPagerAdapter(this, R.layout.fragment_pizza_store, pizzaStoreList)
-
+        pizzaStoreListView.adapter = mPizzaStoreAdapter
 
     }
 
