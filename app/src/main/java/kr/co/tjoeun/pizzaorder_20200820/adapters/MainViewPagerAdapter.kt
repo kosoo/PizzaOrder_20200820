134e237024319e8b40307bdb6a764c41ce8db745
이용찬 <lee9890@naver.com>
2020-08-20 20:21:05 +0900

    메인화면 뷰페이저 어댑터 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.pizzaorder_20200820.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.tjoeun.pizzaorder_20200820.fragments.MyProfileFragment
+import kr.co.tjoeun.pizzaorder_20200820.fragments.PizzaStoreFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> PizzaStoreFragment()
+            else -> MyProfileFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}
