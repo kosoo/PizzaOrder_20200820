cd44c789d248744eba8ea030f97d5ca7bb865604
이용찬 <lee9890@naver.com>
2020-08-20 20:33:19 +0900

    뷰페이저 / 어댑터 연결 작업   - 실제 페이징 처리 완료

--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/MainActivity.kt
@@ -2,12 +2,21 @@ package kr.co.tjoeun.pizzaorder_20200820
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.pizzaorder_20200820.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    //var pizzaStoreList = ArrayList<pizzStore>()
+
+    lateinit var mvpa : MainViewPagerAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //pizzaStoreList.add(pizzStore)
 
         setupEvents()
         setValues()
@@ -18,6 +27,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
     }
 }
+
---
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/datas/PizzaStore.kt
@@ -0,0 +1,7 @@
+package kr.co.tjoeun.pizzaorder_20200820.datas
+
+class PizzaStore(
+
+
+) {
+}
