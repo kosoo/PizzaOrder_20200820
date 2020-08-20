42452a3d56f41375fcabcaa03185723ba7bd1724
이용찬 <lee9890@naver.com>
2020-08-20 21:15:10 +0900

    프래그먼트에서 피자가게 목록을 담아둔 ArrayList 멤버변수 추가

--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/MainActivity.kt
@@ -7,21 +7,11 @@ import kr.co.tjoeun.pizzaorder_20200820.datas.Store
 
 class MainActivity : BaseActivity() {
 
-    var pizzaStoreList = ArrayList<Store>()
-
     lateinit var mvpa : MainViewPagerAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        pizzaStoreList.add(Store("","피자헛", ""))
-        pizzaStoreList.add(Store("","파파존스", ""))
-        pizzaStoreList.add(Store("","도미노 피자", ""))
-        pizzaStoreList.add(Store("","피스터 피자", ""))
-
-        //mvpa = MainViewPagerAdapter(this, R.layout.fragment_pizza_store, pizzaStoreList)
 
         setupEvents()
         setValues()
