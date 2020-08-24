ea7b603445414bec167f70c7421765d80265b303
Choi1979 <cafman004@gmail.com>
2020-08-24 20:20:25 +0900

    프래그먼트로 돌오와서 변경된 닉네임 반영

--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.pizzaorder_20200820.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -36,6 +37,15 @@ class MyProfileFragment : Fragment(){
         }
         
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQ_FOR_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
+                myNickNameTxt.text = data!!.getStringExtra("nick")
+            }
+        }
     }
 
 }
