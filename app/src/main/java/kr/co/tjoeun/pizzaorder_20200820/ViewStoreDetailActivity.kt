f1d3667691525f72ef3c0f911350753112bb97ad
Choi1979 <cafman004@gmail.com>
2020-08-24 19:50:05 +0900

    권한 체크 / 실제 전화 연결 (Call)

--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/ViewStoreDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/ViewStoreDetailActivity.kt
@@ -1,8 +1,14 @@
 package kr.co.tjoeun.pizzaorder_20200820
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import kr.co.tjoeun.pizzaorder_20200820.datas.Store
 
@@ -17,6 +23,28 @@ class ViewStoreDetailActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "전화 연결 권한이 거부", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("[설정] 에서 권한을 열어줘야 전화 연결이 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
 
     }
 
