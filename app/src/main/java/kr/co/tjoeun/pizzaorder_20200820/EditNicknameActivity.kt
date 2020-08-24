74e5e89184655653ffb2a6f68ffa149c925c484c
Choi1979 <cafman004@gmail.com>
2020-08-24 19:59:44 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/EditNicknameActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.pizzaorder_20200820
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
--- app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_20200820/fragments/MyProfileFragment.kt
@@ -1,11 +1,14 @@
 package kr.co.tjoeun.pizzaorder_20200820.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 import kr.co.tjoeun.pizzaorder_20200820.BaseActivity
+import kr.co.tjoeun.pizzaorder_20200820.EditNicknameActivity
 import kr.co.tjoeun.pizzaorder_20200820.R
 
 class MyProfileFragment : Fragment(){
@@ -21,7 +24,12 @@ class MyProfileFragment : Fragment(){
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-//        리스트뷰에 피자 가게 뿌려주기
+//        닉네임 변경 관련 코드 작성 필요
+
+        changeNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(context!!, EditNicknameActivity::class.java)
+        }
         
 
     }
